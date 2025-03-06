22801e211b9a56809489daec6d8d6175189e7593
99mini <itman390@gmail.com>
2025-03-06 13:57:31 +0900

    chore: install dependencies
    * prettier
    * shadcn
    * tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { Button } from '@/components/ui/button';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <main className="h-screen flex flex-col items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold">Hello Vite + React + Tailwind CSS</h1>
+      <p className="text-lg text-gray-500">Edit App.tsx and save to test HMR updates.</p>
+      <p className="text-lg">count: {count}</p>
+      <Button onClick={() => setCount((count) => count + 1)}>count++</Button>
+    </main>
+  );
+};
 
-export default App
+export default App;
