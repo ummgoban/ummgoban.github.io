7869dca10aea068ffde71ee390b63f7a768a65f1
99mini <itman390@gmail.com>
2025-03-07 10:42:18 +0900

    feat: 멘트 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
-import { useState } from 'react';
-import { Button } from '@/components/ui/button';
-
 const App = () => {
-  const [count, setCount] = useState(0);
-
   return (
     <main className="h-screen flex flex-col items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold">Hello Vite + React + Tailwind CSS</h1>
-      <p className="text-lg text-gray-500">Edit App.tsx and save to test HMR updates.</p>
-      <p className="text-lg">count: {count}</p>
-      <Button onClick={() => setCount((count) => count + 1)}>count++</Button>
+      <h1>맘찬픽: 엄고반</h1>
+      <p>소비 기한이 임박한 반찬을 구매하고 픽업하세요!</p>
     </main>
   );
 };
