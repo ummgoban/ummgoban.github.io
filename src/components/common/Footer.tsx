e5cef8dc6ed817511796c4cb6de3a7d8c8f7339b
99mini <43674669+99mini@users.noreply.github.com>
2025-03-12 14:54:21 +0900

    feat: 개인정보이용방침, 이용약관 추가 (#3)
    * feat: 개인정보 이용 약관 수정
    
    * feat: footer 수정
    
    * feat: 연락처 추가

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,20 +1,60 @@
 import { Link } from 'react-router';
 
+const FooterItem = ({ children, title }: { title: string; children: React.ReactNode }) => {
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="font-semibold text-lg">{title}</div>
+      {children}
+    </div>
+  );
+};
+
 const Footer = () => {
   return (
     <footer className="w-full bg-gray-500 py-8 mt-auto">
-      <div className="container mx-auto px-4">
-        <div className="font-semibold text-lg mb-4">고객지원</div>
-        <ul className="space-y-2">
-          <li className="flex space-x-4">
-            <Link to="/terms" className="text-gray-800 hover:text-blue-400 hover:underline cursor-pointer">
-              서비스 이용약관
-            </Link>
-            <Link to="/privacy" className="text-gray-800 hover:text-blue-400 hover:underline cursor-pointer">
-              개인정보 처리방침
-            </Link>
-          </li>
-        </ul>
+      <div className="flex flex-col">
+        <div className="container mx-auto px-4 grid sm:grid-cols-3 sm:grid-rows-1 grid-rows-3 grid-cols-1">
+          <FooterItem title="고객지원">
+            <ul className="space-y-2">
+              <li className="flex space-x-4">
+                <Link
+                  to="/terms"
+                  className="text-gray-800 underline hover:text-gray-600 hover:underline cursor-pointer"
+                >
+                  서비스 이용약관
+                </Link>
+              </li>
+              <li className="flex space-x-4">
+                <Link
+                  to="/privacy"
+                  className="text-gray-800 underline hover:text-gray-600 hover:underline cursor-pointer"
+                >
+                  개인정보 처리방침
+                </Link>
+              </li>
+            </ul>
+          </FooterItem>
+          <FooterItem title="서비스">
+            <ul className="space-y-2">
+              <li className="flex space-x-4"></li>
+            </ul>
+          </FooterItem>
+          <FooterItem title="Conenect Us">
+            <ul className="space-y-2">
+              <li className="flex space-x-4">
+                <a
+                  href="mailto:momchanpick@gmail.com"
+                  className="text-gray-800 underline hover:text-gray-600 hover:underline cursor-pointer"
+                >
+                  momchanpick@gmail.com
+                </a>
+              </li>
+            </ul>
+          </FooterItem>
+        </div>
+        <div className="flex items-center justify-center mt-4">
+          <div className="text-gray-800">{`© ${new Date().getFullYear()} 맘찬픽, All Rights Reserved.`}</div>
+        </div>
       </div>
     </footer>
   );
