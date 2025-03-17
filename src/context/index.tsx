25c070c10b818ece6cab9aa649cbfdf17b7f5ef3
99mini <itman390@gmail.com>
2025-03-17 23:13:26 +0900

    fix: base url

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter } from 'react-router';
 
 const RootProvider = ({ children }: { children: React.ReactNode }) => {
-  return <BrowserRouter basename={import.meta.env.PUBLIC_URL}>{children}</BrowserRouter>;
+  return <BrowserRouter basename={import.meta.env.BASE_URL}>{children}</BrowserRouter>;
 };
 
 export default RootProvider;
--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,4 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: '',
 });
